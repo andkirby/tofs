475c6ee695c8671814b20d42ef6654f0ad108a64
Andrew Roslik <andkirby@gmail.com>
2016-05-07 22:42:53 +0300

    Added slack_sender.

---
+++ lib/service/sender/slack_sender.rb
@@ -0,0 +1,38 @@
+require_relative 'sender_abstract'
+require 'slack-notifier'
+require 'openssl'
+
+module Service
+  module Sender
+    class SlackSender < SenderAbstract
+      ##
+      # Send message to slack webhook
+      #
+      # @param [String] message
+      # @param [Hash] options
+      # @return [Net::HTTPResponse]
+      #
+      def send(message, options = {})
+        return until message
+
+        init_client.ping message
+      end
+
+      ##
+      # Init slack notifier
+      #
+      # @return [SlackSender::Notifier]
+      #
+      def init_client
+        notifier = Slack::Notifier.new @options[:webhook_url]
+        if @options[:channel]
+          notifier.channel = @options[:channel]
+        end
+        if @options[:username]
+          notifier.username = @options[:username]
+        end
+        notifier
+      end
+    end
+  end
+end
