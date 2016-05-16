983a6e863a062c3d9fdc8ba778f06672de960d07
Andrew Roslik <andkirby@gmail.com>
2016-05-16 21:21:47 +0300

    Set 3600s cache requests time.

--- lib/service/api/request.rb
+++ lib/service/api/request.rb
@@ -5,9 +5,16 @@ require_relative 'cacher'
 module Service
   module Api
     module Request
-      def request(url, use_cache = true, namespace = nil)
+      def request(url, use_cache = true, namespace = nil, timeout = 3600)
+        use_cache = false if timeout == 0
+
         if use_cache
-          cacher = Api::Cacher.new({:base_name => URI.parse(url).host})
+          cacher = Api::Cacher.new(
+            {
+              :base_name => URI.parse(url).host,
+              :timeout => timeout,
+            }
+          )
           body = cacher.get url, namespace
           return body if nil != body
 
