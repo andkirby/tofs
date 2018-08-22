a06552d25a281a0d70f9ba3549921534651fde4b
Andrew Roslik <andkirby@gmail.com>
2018-08-22 22:59:19 +0300

    Added different sections to CLI menu.

--- src/bmovies.rb
+++ src/bmovies.rb
@@ -1,10 +1,12 @@
 require 'pp'
 require 'highline/import'
 require 'colorize'
+require 'terminal-table'
+require 'word_wrap'
 
 require_relative 'lib/service/bmovies/api/category/menu'
-require_relative 'lib/service/bmovies/api/category/years'
-require_relative 'lib/service/bmovies/api/category/genres'
+require_relative 'lib/service/bmovies/api/item/slider'
+require_relative 'lib/service/bmovies/api/item/movie'
 require_relative 'lib/shell/output'
 
 module Bmovies
@@ -13,30 +15,80 @@ module Bmovies
   def run
     Shell::Output::simple Service::Bmovies::get_base_url
 
-    # choose menu
-    menu_item = request_menu_item
+    values = ['Menu', 'Slides', 'Movie']
 
-    url = menu_item[:url]
+    choice = choose_menu(values, question: 'Choose entity?')
+    case choice
+    when 'Menu'
+      show_entity request_menu_item
+    when 'Slides'
+      request_slides.each {|el| show_entity el}
+    when 'Movie'
+      show_entity request_movie(
+                      # '/film/alpha.5kx30/r1x84o'
+                      reask('Please input movie URI?', nil).to_s
+                  )
+    else
+      raise "Unknown option: #{choice}."
+    end
   end
 
+  def request_slides
+    Shell::Output::simple 'Home slides'.green
+
+    Service::Bmovies::Api::Item::Slider.new.fetch
+  end
+
+  ##
+  # Fetch movie data
+  def request_movie(uri)
+    Shell::Output::simple 'Movie'.green
+
+    Service::Bmovies::Api::Item::Movie.new.fetch uri: uri
+  end
+
+  ##
+  # Fetch menu
   def request_menu_item
-    Shell::Output::simple 'Categories:'.green
+    Shell::Output::simple 'Menu Categories'.green
 
-    show_menu Service::Bmovies::Api::Category::Menu.new.fetch
+    choose_menu Service::Bmovies::Api::Category::Menu.new.fetch, question: 'Which section?'
   end
 
-  def show_menu(list)
+  def choose_menu(list, question:)
     ii = 0
     list.each {|el|
       ii += 1
-      Shell::Output::simple ii.to_s + ': ' + el[:label]
+      Shell::Output::simple ii.to_s + ': ' +
+                                (el.kind_of?(String) ? el : el[:label])
     }
     values = [*1..ii].map(&:to_s)
-    id = reask('Which section?', values).to_i - 1
+    id = reask(question, values).to_i - 1
 
-    return show_menu list[id][:_children] if list[id][:_children]
+    return choose_menu list[id][:_children], question: question if list[id].kind_of?(Hash) and list[id][:_children]
 
     list[id]
+  end
+
+  ##
+  # Show entity data
+  def show_entity(entity, name: nil, max_value_with: 100, max_width: 120)
+    rows = []
+    entity.each do |key, value|
+      if value.kind_of? Hash
+        value = value[:label]
+      end
+      if value.kind_of? Array and value.first.kind_of? Hash and !value.first[:label].nil?
+        value = value.collect{|h| h[:label]}.join(", ").to_s
+      end
+      # words wrap, max 80 symbols
+      value = WordWrap.ww value, max_value_with
+      rows.push [key, value]
+    end
+
+    table = Terminal::Table.new :title => name, :rows => rows, style: {:padding_left => 2, :width => max_width}
+
+    puts table
   end
 
   ##
@@ -51,6 +103,9 @@ module Bmovies
     Shell::Output::simple question.yellow
     Shell::Output::simple '(' + answers.join(', ') + ')' if show_values
     input = ask '> '
+
+    return input unless answers
+
     unless answers && answers.include?(input)
       failures -= 1
       if failures < 1
